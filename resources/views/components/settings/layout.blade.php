82f4c6d132d5cf7519aa533ef7b3862c77df8dac
marius.oppolzer <mariusoppolzer@googlemail.com>
2026-02-02 02:04:18 +0100

    donnnne

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -13,10 +13,7 @@
     <flux:separator class="md:hidden" />
 
     <div class="flex-1 self-stretch max-md:pt-6">
-        <flux:heading>{{ $heading ?? '' }}</flux:heading>
-        <flux:subheading>{{ $subheading ?? '' }}</flux:subheading>
-
-        <div class="mt-5 w-full max-w-lg">
+        <div class="w-full max-w-lg">
             {{ $slot }}
         </div>
     </div>
